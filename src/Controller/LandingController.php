ed2ffe4614e3642fec598b276f29f81dd8bbd53e
Fran <fsanpaj255@g.educaand.es>
2023-05-01 19:34:43 +0200

    v18

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -1,7 +1,7 @@
 <?php
 
 namespace App\Controller;
-
+use App\Repository\ProductoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +9,10 @@ use Symfony\Component\Routing\Annotation\Route;
 class LandingController extends AbstractController
 {
     #[Route('/', name: 'app_landing')]
-    public function index(): Response
+    public function listadoJuegos(ProductoRepository $productosRepository): Response
     {
         return $this->render('landing/index.html.twig', [
-            'controller_name' => 'LandingController',
+            'listaproductos' => $productosRepository->findAll(),
         ]);
     }
 }

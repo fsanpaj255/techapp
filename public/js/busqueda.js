9aa5eeebc23bd9dae46006b576b839d6cde2f341
Fran <fsanpaj255@g.educaand.es>
2023-05-18 23:23:21 +0200

    v28

--- public/js/busqueda.js
+++ public/js/busqueda.js
@@ -1,59 +1,48 @@
 $(document).ready(function() {
-    $(".search-btn").click(function(event) {
+  $(".search-btn").click(function(event) {
       event.preventDefault();
-  
-      var categoria = $(".input-select").val();
+
       var nombre = $(".input").val();
-  
+
       // Realizar la solicitud GET utilizando AJAX
       $.ajax({
-        url: "http://localhost:8000/api/producto/buscar/" + nombre,
-        type: "GET",
-        success: function(response1) {
-          // Obtener la categoría del producto encontrado
-          var categoriaProducto = response.categoria;
-  
-          // Verificar si la categoría seleccionada coincide con la categoría del producto encontrado
-          if (categoria === categoriaProducto) {
-            // Preparar datos del producto buscado
-            var productoBuscado = {
-              id: response.id,
-              precio: response.precio,
-              nombre: response.nombre,
-              descripcion: response.descripcion,
-              tamano: response.tamano,
-              modelo: response.modelo,
-              color: response.color,
-              peso: response.peso,
-              imageName: response.imageName,
-              categoria: response.categoria
-            };
-            console.log(productoBuscado);
-            // Realizar otra solicitud GET para obtener productos con la misma categoría
-            $.ajax({
-              url: "http://localhost:8000/api/producto/categoria/" + categoria,
-              type: "GET",
-              success: function(response2) {
-                // Obtener los productos con la misma categoría
-                var productosCategoria = response;
-  
-                // Agregar el producto buscado a la lista de productos de la misma categoría
-                productosCategoria.push(productoBuscado);
-  
-                // Hacer algo con los productos obtenidos
-                console.log(productosCategoria);
-              },
-              error: function(xhr, status, error) {
-                console.log("Error al obtener productos por categoría: " + error);
-              }
-            });
-          } else {
-            console.log("La categoría seleccionada no coincide con la categoría del producto encontrado.");
+          url: "http://localhost:8000/api/producto/buscar/" + nombre,
+          type: "GET",
+          success: function(response) {
+              // Preparar datos del producto buscado
+              var productoBuscado = {
+                  id: response.id,
+                  precio: response.precio,
+                  nombre: response.nombre,
+                  descripcion: response.descripcion,
+                  tamano: response.tamano,
+                  modelo: response.modelo,
+                  color: response.color,
+                  peso: response.peso,
+                  imageName: response.imageName,
+                  categoria: response.categoria
+              };
+              console.log(productoBuscado);
+              
+              // Enviar los datos a la ruta del controlador en Symfony
+              $.ajax({
+                  url: "http://localhost:8000/resultado/busqueda",
+                  type: "POST",
+                  data: {
+                      datos: [productoBuscado]
+                  },
+                  success: function() {
+                      // Redirigir a la pantalla Twig de resultados
+                      window.location.href = "http://localhost:8000/resultado/busqueda";
+                  },
+                  error: function(xhr, status, error) {
+                      console.log("Error al enviar los datos al controlador: " + error);
+                  }
+              });
+          },
+          error: function(xhr, status, error) {
+              console.log("Error al buscar el producto: " + error);
           }
-        },
-        error: function(xhr, status, error) {
-          console.log("Error al buscar el producto: " + error);
-        }
       });
-    });
   });
+});
--- src/Controller/ResultadoBusquedaController.php
+++ src/Controller/ResultadoBusquedaController.php
@@ -3,16 +3,22 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class ResultadoBusquedaController extends AbstractController
 {
-    #[Route('/resultado/busqueda', name: 'app_resultado_busqueda')]
-    public function index(): Response
+    #[Route("/resultado/busqueda", name: "resultado_busqueda")]
+    public function mostrarResultadoBusqueda(Request $request): Response
     {
+        // Obtener los datos enviados desde jQuery
+        $datos = $request->get('datos');
+
+        // Renderizar la plantilla Twig y pasar los datos como variables
         return $this->render('resultado_busqueda/index.html.twig', [
-            'controller_name' => 'ResultadoBusquedaController',
+            'datos' => $datos,
         ]);
     }
 }

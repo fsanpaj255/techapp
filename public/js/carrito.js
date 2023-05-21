f7bf5cbc0ce81b8c58a2b147d087dbb555f27e6f
Fran <fsanpaj255@g.educaand.es>
2023-05-21 19:25:30 +0200

    v35

--- public/js/carrito.js
+++ public/js/carrito.js
@@ -1,5 +1,4 @@
-
-  function addToCart(productId) {
+function addToCart(productId) {
     $.ajax({
       url: 'http://localhost:8000/api/producto/get/' + productId,
       type: 'GET',
@@ -12,9 +11,17 @@
         console.log(producto.nombre);
         console.log(producto.precio);
   
-          // Crear los elementos HTML del producto
-          var productWidget = $('<div class="product-widget"></div>');
-          var productImg = $('<div class="product-img"><img src="./img/'+productoimagen+'" alt=""></div>');
+        // Verificar si el producto ya está en el carrito
+        var productWidget = $('.cart-list').find('[data-product-id="' + productId + '"]');
+        if (productWidget.length > 0) {
+          // El producto ya está en el carrito, incrementar la cantidad
+          var quantityElement = productWidget.find('.qty');
+          var currentQuantity = parseInt(quantityElement.text());
+          quantityElement.text((currentQuantity + 1) + 'x');
+        } else {
+          // El producto no está en el carrito, crear los elementos HTML
+          var productWidget = $('<div class="product-widget" data-product-id="' + productId + '"></div>');
+          var productImg = $('<div class="product-img"><img src="./img/' + productoimagen + '" alt=""></div>');
           var productBody = $('<div class="product-body"></div>');
           var productNameElement = $('<h3 class="product-name"><a href="#">' + productonombre + '</a></h3>');
           var productPriceElement = $('<h4 class="product-price"><span class="qty">1x</span>$' + productoprecio + '</h4>');
@@ -29,10 +36,10 @@
   
           // Agregar el producto al carrito en el div "cart-list"
           $('.cart-list').append(productWidget);
-        },
-        error: function(error) {
-          // Manejar el error de la llamada AJAX aquí
         }
-      });
-    }
-
+      },
+      error: function(error) {
+        // Manejar el error de la llamada AJAX aquí
+      }
+    });
+  }

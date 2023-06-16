0ff3c2c43a6a5d22f296db88255f1a250f802c25
Fran <fsanpaj255@g.educaand.es>
2023-06-16 15:30:17 +0200

    v94

--- src/Controller/CorreoController.php
+++ src/Controller/CorreoController.php
@@ -4,26 +4,23 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-
-use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CorreoController extends AbstractController
 {
-    #[Route('/email')]
-    public function sendEmail(MailerInterface $mailer): Response
+    #[Route('/correo', name: 'app_correo')]
+    public function enviarcorreo(MailerInterface $mailer)
     {
         $email = (new Email())
-        ->from('santoyopajaresf@gmail.com')
-        ->to('santoyopajaresf@gmail.com')
-        ->subject('Asunto del correo')
-        ->text('Contenido del correo.');
+        ->from('fcotrabajos8@gmail.com')
+        ->to('fcotrabajos8@gmail.com')
+        ->subject('Asunto del correo');
 
     $mailer->send($email);
 
-    return new Response('ENVIADO',Response::HTTP_BAD_REQUEST);
+    return $this->redirectToRoute('app_landing');
 
     }
 }

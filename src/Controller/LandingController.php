6060745c408624473ae2f400a92cba1b19de6f24
Fran <fsanpaj255@g.educaand.es>
2023-06-18 21:48:33 +0200

    v102

--- src/Controller/LandingController.php
+++ src/Controller/LandingController.php
@@ -1,23 +1,30 @@
 <?php
 
 namespace App\Controller;
+
 use App\Repository\ProductoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Doctrine\ORM\EntityManagerInterface;
-use Symfony\Component\Serializer\Encoder\JsonEncoder;
-use Symfony\Component\Serializer\SerializerInterface;
 
 class LandingController extends AbstractController
 {
     #[Route('/', name: 'app_landing')]
-    public function listadoJuegos(ProductoRepository $productosRepository): Response
+    public function listadoJuegos(Request $request, ProductoRepository $productoRepository): Response
     {
+        $categoria = $request->query->get('categoria');
+
+        if ($categoria) {
+            $productos = $productoRepository->findBy(['categoria' => $categoria]);
+        } else {
+            $productos = $productoRepository->findAll();
+        }
+
         return $this->render('landing/index.html.twig', [
-            'listaproductos' => $productosRepository->findAll(),
+            'listaproductos' => $productos,
+            'categoriaSeleccionada' => $categoria,
         ]);
     }
-    
-
 }
+

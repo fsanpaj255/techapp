492acafd3f4ab7913eb09304fc7822b670efb2bb
Fran <fsanpaj255@g.educaand.es>
2023-06-20 03:10:42 +0200

    v122

--- src/Controller/editarmisdatosController.php
+++ src/Controller/editarmisdatosController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use App\Form\UsuarioType;
+use App\Form\UsuarioType2;
 
 class editarmisdatosController extends AbstractController
 {
@@ -19,18 +19,31 @@ class editarmisdatosController extends AbstractController
     }
     
     #[Route('/editarmiperfil', name: 'app_editarmiperfil')]
-    public function editarDatos(Request $request): Response
+    public function mostrarFormulario(Request $request): Response
     {
         $usuario = $this->getUser();
         
-        // Crear el formulario y pasar los datos del usuario actual
-        $form = $this->createForm(UsuarioType::class, $usuario);
+        // Crear el formulario y pasar los datos del usuario actual a este nmmismo 
+        $form = $this->createForm(UsuarioType2::class, $usuario);
+        
+        return $this->render('perfilusuario/editardatos.html.twig', [
+            'form' => $form->createView(),
+            'usuario' => $usuario,
+        ]);
+    }
+    
+    #[Route('/actualizardatos', name: 'app_actualizar_datos', methods: ['POST'])]
+    public function actualizarDatos(Request $request): Response
+    {
+        $usuario = $this->getUser();
+        
+        // Crear el formulario y pasar los datos del usuario actual a este nmmismo
+        $form = $this->createForm(UsuarioType2::class, $usuario);
         
         $form->handleRequest($request);
         
         if ($form->isSubmitted() && $form->isValid()) {
             // Guardar los cambios en la base de datos
-            $this->entityManager->persist($usuario);
             $this->entityManager->flush();
             
             // Redirigir a la página de perfil después de guardar los cambios
